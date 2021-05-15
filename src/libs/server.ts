3a5ca80f4da445e05a2869d61b93438e861b01ca
Guillaume <hi@255kb.dev>
2021-05-15 09:52:05 +0200

    Update CI to Node.js 14

--- src/libs/server.ts
+++ src/libs/server.ts
@@ -490,9 +490,12 @@ export class MockoonServer extends (EventEmitter as new () => TypedEmitter<Serve
               request
             );
 
-            if (this.environment.proxyRemovePrefix === true && this.environment.endpointPrefix.length > 0) {
+            if (
+              this.environment.proxyRemovePrefix === true &&
+              this.environment.endpointPrefix.length > 0
+            ) {
               const regExp = new RegExp(`^/${this.environment.endpointPrefix}`);
-              proxyReq.path = proxyReq.path.replace(regExp,'');
+              proxyReq.path = proxyReq.path.replace(regExp, '');
             }
 
             // re-stream the body (intercepted by body parser method)
