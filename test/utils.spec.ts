0f5f44d47b529ca4a0602a2c2503fcd6eb2b0bd8
devindad <a.d.delgoda@gmail.com>
2021-05-19 10:58:34 +0800

    Closes #479

--- test/utils.spec.ts
+++ test/utils.spec.ts
@@ -1,5 +1,8 @@
 import { expect } from 'chai';
-import { ToBase64 } from '../src/libs/utils';
+import { ToBase64, DecompressBody } from '../src/libs/utils';
+import { Response } from 'express';
+import fs from 'fs';
+
 
 describe('Utils', () => {
   describe('toBase64', () => {
@@ -21,4 +24,43 @@ describe('Utils', () => {
       (global.btoa as unknown) = undefined;
     });
   });
+
+  describe('DecompressBody', () => {
+    it('should decompress gzip encoded data', () => {
+      const response = {
+        'getHeader': (_: any) => 'gzip',
+        'body': fs.readFileSync(`${__dirname}/resources/content-encoding/gzip.data`)
+      };
+
+      expect(DecompressBody(response as Response)).to.equal('gzipTest');
+    });
+
+    it('should decompress brotli encoded data', () => {
+      const response = {
+        'getHeader': (_: any) => 'br',
+        'body': fs.readFileSync(`${__dirname}/resources/content-encoding/br.data`)
+      };
+
+      expect(DecompressBody(response as Response)).to.equal('brTest');
+    });
+
+    it('should decompress deflate encoded data', () => {
+      const response = {
+        'getHeader': (_: any) => 'deflate',
+        'body': fs.readFileSync(`${__dirname}/resources/content-encoding/deflate.data`)
+      };
+
+      expect(DecompressBody(response as Response)).to.equal('deflateTest');
+    });
+
+    it('should handle plain data', () => {
+      const response = {
+        'getHeader': (_: any) =>undefined,
+        'body': fs.readFileSync(`${__dirname}/resources/content-encoding/plain.data`)
+      };
+
+      expect(DecompressBody(response as Response)).to.equal('plainTest');
+    });
+  });
+
 });
