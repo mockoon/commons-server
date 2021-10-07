47de9477b702534dd0242758b3dd0fe41cd943aa
Guillaume <hi@255kb.dev>
2021-10-07 16:40:00 +0200

    Set version 2.11.0 and update deps

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -16,6 +16,16 @@ declare module 'http' {
   export interface Server {
     kill: (callback: () => void) => void;
   }
+  export interface IncomingMessage {
+    proxied: boolean;
+    rawBody: Buffer;
+    parsedBody: any;
+  }
+  export interface ServerResponse {
+    body: any;
+    routeUUID: string;
+    routeResponseUUID: string;
+  }
 }
 
 declare module 'https' {
